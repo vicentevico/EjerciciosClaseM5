3d845bccce7b18361fb75f216a7f7cc41ad14765
Vicente <Vicente@Vicente-PC>
2016-09-15 21:49:30 +0200

    123

--- EjercicioDePrueba/src/Prueba1.java
+++ EjercicioDePrueba/src/Prueba1.java
@@ -4,6 +4,7 @@ public class Prueba1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ejercicio de prueba");
+		System.out.println("Hola");
 
 	}
 
